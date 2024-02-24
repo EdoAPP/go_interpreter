5a4f3958c4317cdb1ec31ec3a313477a8fb28473
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-02-24 17:44:42 -0500

    Add initial parser with let stement support

---
+++ parser/parser.go
@@ -0,0 +1,104 @@
+package parser
+
+import (
+	"fmt"
+	"go-interpreter/ast"
+	"go-interpreter/lexer"
+	"go-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	errors []string
+
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l, errors: []string{}}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: Skipping expressions until find semicolon. Just evaluating assign expression for now
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) currTokenIs(tt token.TokenType) bool {
+	return p.currToken.Type == tt
+}
+
+func (p *Parser) peekTokenIs(tt token.TokenType) bool {
+	return p.peekToken.Type == tt
+}
+
+func (p *Parser) expectPeek(tt token.TokenType) bool {
+	if p.peekTokenIs(tt) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(tt)
+		return false
+	}
+}
+
+func (p *Parser) peekError(tt token.TokenType) {
+	msg := fmt.Sprintf("Expected token to be %s, got %s instead", tt, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
