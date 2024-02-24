290a8b209a1b32ef5e484ca40cabe179b6e0f1da
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-02-24 13:30:31 -0500

    Initial ast struct

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "go-interpreter/token"
+
+// the starting node of the ast tree
+type Program struct {
+	Statements []Statement
+}
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	// even though this "Identifier" expression doesn't return a value in a let Name Statement
+	// an `Identifier` can indeed produce a value when let x = valueProducingIdentifier for example
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
