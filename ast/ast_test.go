34650e2500f25f11371eec2a09d7574711ed4227
Eduardo Pacheco <eduardoapp.97@gmail.com>
2024-03-07 23:31:28 -0500

    add string method for easier debugging of expressions

---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"go-interpreter/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "testVar"},
+					Value: "testVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let testVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
